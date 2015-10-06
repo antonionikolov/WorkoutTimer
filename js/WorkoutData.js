ed1973e6041bb645c29a5b14f517771eec5318c9
unknown <antonio.nikolov@x120e-R-4M0C5.musala.com>
2015-10-06 18:19:05 +0300

    Adds info about the round and exercises

--- js/WorkoutData.js
+++ js/WorkoutData.js
@@ -1,23 +1,37 @@
 var WorkoutData = function (rounds, exercises, exerciseTime, exerciseRest, roundRest) {
 	this.rounds           = rounds;
-	this.exercises        = exercises * 2 - 1;
+	this.exercises        = exercises * 2 - 1; // exercises + rest
 	this.currentExercises = this.exercises;
 	this.exerciseTime     = exerciseTime;
 	this.exerciseRest     = exerciseRest;
 	this.roundRest        = roundRest;
+	this.roundCount      = 1;
+	this.exerciseCount   = 0;
 }
 
 WorkoutData.prototype.next = function() {
 	if (this.currentExercises == 0) {
 		this.currentExercises = this.exercises;
 		this.rounds--;
+		this.roundCount++;
+		this.exerciseCount = 0;
+		$('#infoRound').text('Round ' + this.roundCount);
+		$('#infoExercise').text('Long rest');
+		$('#timer').css('color', '#3225E4');
 		return this.roundRest;
 	}
 	if (this.currentExercises % 2 == 0) {
 		this.currentExercises--;
+		$('#infoRound').text('Round ' + this.roundCount);
+		$('#infoExercise').text('Rest');
+		$('#timer').css('color', '#3225E4');
 		return this.exerciseRest;
 	} else {
 		this.currentExercises--;
+		this.exerciseCount++;
+		$('#infoRound').text('Round ' + this.roundCount);
+		$('#infoExercise').text('Exercise ' + this.exerciseCount);
+		$('#timer').css('color', '#FF0000');
 		return this.exerciseTime;
 	}
 };
